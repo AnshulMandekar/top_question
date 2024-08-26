367d2943590e4ef7e056944b4007d2fe295ea6b7
AnshulMandekar <148358538+AnshulMandekar@users.noreply.github.com>
2024-08-26 20:59:05 +0530

    Create removeLoop.cpp

---
+++ removeLoop.cpp
@@ -0,0 +1,18 @@
+Node *removeLoop(Node *head)
+{
+    if(head == NULL ){
+        return false;
+    }    
+    
+    map<Node* , bool> visited;
+    Node* temp = head;
+    
+    while(temp != NULL){
+        if(visited[temp] == true){
+            return 1;
+        }
+        visited[temp] = true;
+        temp = temp->next;
+    }
+    return false;
+}
