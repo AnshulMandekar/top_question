1fc1efc703c0ec789228d8896d21a395562d18e0
AnshulMandekar <148358538+AnshulMandekar@users.noreply.github.com>
2024-08-27 19:29:32 +0530

    Update floydetectloop.cpp

--- floydetectloop.cpp
+++ floydetectloop.cpp
@@ -17,3 +17,36 @@ Node* floydetectloop(Node* head){
     }
     return 0;
 }
+
+Node* floydetectloop(Node* head){
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL){
+            return 0;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while( fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+
+
+            slow = slow->next;
+            if(slow == fast){
+                return 1;
+            }
+        }
+        return 0;
+        
+    }
+};
